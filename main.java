e008462949543e7f30632e037b4783e945582c58
MUNTSIR <montyhamid7@gmail.com>
2026-06-23 03:10:30 +0000

    Finish TUTORIAL 3

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        person P1 = new person("hsisw", "1001");
+        person P2 = new student("alice", "3563");
+
+        P1.introduce();
+        P2.introduce();
+    }
+}
---
+++ student.java
@@ -0,0 +1,10 @@
+public class student extends person {
+    public student(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("I am a student.");
+    }
+}
